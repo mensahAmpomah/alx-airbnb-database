055f4a6414e425629deed3141fcd6c0f63356299
mensahAmpomah <gideonmensah201@gmail.com>
2025-10-29 03:05:16 -0700

    Rank and Row added

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,22 +1,16 @@
-Write a query to find the total number of bookings made by each user, using the COUNT function and GROUP BY clause.
-
 SELECT 
-    User.user_id,
-    User.first_name AS firstname,
-    User.last_name AS lastname,
-    COUNT(Booking.booking_id) AS total_bookings
-FROM User
-LEFT JOIN Booking 
-    ON User.user_id = Booking.user_id
+    p.property_id,
+    p.name AS property,
+    COUNT(b.booking_id) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
+FROM Property p
+LEFT JOIN Booking b
+    ON p.property_id = b.property_id
 GROUP BY 
-    User.user_id, 
-    User.first_name, 
-    User.last_name
+    p.property_id,
+    p.name
 ORDER BY 
-    total_bookings DESC;
-
-
-Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
+    property_rank;
 
 
 SELECT 
@@ -27,8 +21,8 @@ SELECT
 FROM Property p
 LEFT JOIN Booking b
     ON p.property_id = b.property_id
-GROUP BY
+GROUP BY 
     p.property_id,
     p.name
-ORDER BY
+ORDER BY 
     property_rownum;
